24a794b9a40dbe54c2cd7cf80f85a77560f1cd14
hraya <hraya@lssu.edu>
2019-08-19 20:54:56 -0500

    Completed the counter

--- assignments/closure.js
+++ assignments/closure.js
@@ -21,11 +21,18 @@ theState()
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 1;
+  const addUno = () => {
+    console.log(count++)
+  }
+  return addUno
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
 
+// Example usage: 
+const newCounter = counter();
+newCounter(); // 1
+ newCounter(); // 2
+ newCounter(); // 3
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
